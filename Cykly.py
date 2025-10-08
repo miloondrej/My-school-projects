adb23fb7e55ad9d78dc69f01e57259be2f84dbee
Milo Ondrej <miloondrej@gmail.com>
2025-10-08 08:25:31 +0200

    8.10.25 - 1

---
+++ Cykly.py
@@ -0,0 +1,6 @@
+import time 
+x=0
+while x<5:            # cyklus se opakuje, dokud je x menší než 5
+    print(x)
+    x +=1             # zvýšení hodnoty x o 1
+    time.sleep(0.5)   # pauza 0,5 sekunda
