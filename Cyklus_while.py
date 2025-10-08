e23e085b5a83049738ab6be772ee054d5898f019
Milo Ondrej <miloondrej@gmail.com>
2025-10-08 10:43:56 +0200

    8/10/2025 - 4

--- Cyklus_while.py
+++ Cyklus_while.py
@@ -4,7 +4,7 @@ while x<5:            # cyklus se opakuje, dokud je x menší než 5
     print(x)
     x +=1             # zvýšení hodnoty x o 1
     time.sleep(0.5)   # pauza 0,5 sekunda
-while x>0:            # cyklus se opakuje, dokud je x větší než 0
+while x>=0:            # cyklus se opakuje, dokud je x větší než 0
     print(x)
     x -=1             # snížení hodnoty x o 1
     time.sleep(0.5)   # pauza 0,5 sekunda
---
+++ print_text_na_vice_radku.py
@@ -0,0 +1,2 @@
+print ("Pokusný text \nna více řádků")  #\n = nový řádek, \t = tabulátor
+print ("Pokusný text \tna více řádků")  #\n = nový řádek, \t = tabulátor
