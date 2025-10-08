88934dd35df1e43fd5f469e55939ac26299568ee
Milo Ondrej <miloondrej@gmail.com>
2025-10-08 08:39:16 +0200

    8.10.25 - 2

--- Cykly.py
+++ Cykly.py
@@ -1,6 +1,10 @@
-import time 
+import time           # import knihovny time pro pauzu
 x=0
 while x<5:            # cyklus se opakuje, dokud je x menší než 5
     print(x)
     x +=1             # zvýšení hodnoty x o 1
+    time.sleep(0.5)   # pauza 0,5 sekunda
+while x>0:            # cyklus se opakuje, dokud je x větší než 0
+    print(x)
+    x -=1             # snížení hodnoty x o 1
     time.sleep(0.5)   # pauza 0,5 sekunda
